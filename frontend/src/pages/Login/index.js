cfe15940dfd8ccbee4fc4cce53deff9642dc2a1a
Matheus <matheusrodrigofilho@outlook.com>
2020-10-25 09:20:27 -0300

    Adicionando Estilo na pagina de login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -5,31 +5,32 @@ import Header from '../components/Header/index.js';
 import Footer from '../components/Footer/index.js';
 
 //images
-import noImage from '../../images/noImage.png'
+import noImage from '../../images/noImage.png';
+
+import './style.css';
 
 function Login() {
   return (
     <div>
       <Header />
-      <main style={{marginTop:120}}>
-          <div>
-            <img src={noImage} alt="Zarpar" className="logo"/>
+      <main style={{marginTop:120}} className="content">
+          <div className="imageContainer">
+            <img src={noImage} alt="Zarpar" className="image"/>
           </div>
-          <div>
-              <h1>Login</h1>
-              <form>
-                <label>
-                    Email:
-                    <input type="email" name="email" />
-                </label>
-                <label>
-                    Senha:
-                    <input type="password" name="password" />
-                </label>
-                <Link to="/"><p>Esqueceu a senha?</p></Link>
-                <input type="submit" value="Enviar" />
+          <div className="formContainer">
+              <h1 className="formTitle">Login</h1>
+              <form className="form">
+                <input type="text" name="user" className="formInput" placeholder="CPF/CNPJ" />
+                <br />
+                <input type="password" name="password" className="formInput" placeholder="Senha" />
+                <div className="textLinkPasswordContainer">
+                    <Link to="/" className="textLinkPassword"><p>Esqueceu a senha?</p></Link>
+                </div>
+                <div className="sendButtonContainer">
+                    <input type="submit" value="Entrar" className="sendButton"/>
+                </div>
             </form>
-            <Link to="/"><p>Ainda não tem um Cadastro? a gente já resolve isso</p></Link>
+            <Link to="/" className="textLinkRegister"><p>Ainda não tem um Cadastro? a gente já resolve isso</p></Link>
                               
           </div>
       </main>
