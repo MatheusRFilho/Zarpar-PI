61de383a1b501aab970af7d9137f5d1b739513e3
Gabriel Pires <gabriel.santos.pes@gmail.com>
2020-10-26 16:15:36 -0300

    Sobre nós

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -6,6 +6,9 @@ import Footer from '../components/Footer';
 
 import './styles.css';
 
+const topicsLogo =
+  'https://i.picsum.photos/id/599/200/200.jpg?hmac=2WLKs3sxIsaEQ-6WZaa6YMxgl6ZC4cNnid0aqupm2is';
+
 function Home() {
   return (
     <div>
@@ -29,6 +32,28 @@ function Home() {
           </div>
         </section>
         <hr />
+        <section className="aboutUs">
+          <input className="searchInput" placeholder="Pesquisar" />
+          <h1 className="titleAboutUs">Sobre nós</h1>
+          <div className="topics">
+            <div className="topic">
+              <img src={topicsLogo} alt="Imagem do tópico" />
+              <h4>Lorem</h4>
+            </div>
+            <div className="topic">
+              <img src={topicsLogo} alt="Imagem do tópico" />
+              <h4>Lorem</h4>
+            </div>
+            <div className="topic">
+              <img src={topicsLogo} alt="Imagem do tópico" />
+              <h4>Lorem</h4>
+            </div>
+            <div className="topic">
+              <img src={topicsLogo} alt="Imagem do tópico" />
+              <h4>Lorem</h4>
+            </div>
+          </div>
+        </section>
       </div>
       <Footer />
     </div>
