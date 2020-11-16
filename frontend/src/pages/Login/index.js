054ce22622dc7918de53f46329d1bd42b9b5fe1d
Matheus <matheusrodrigofilho@outlook.com>
2020-11-16 05:35:08 -0300

    Mensagens de validação e Checagem do login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -4,12 +4,33 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import { ErrorMessage, Formik, Form, Field} from 'formik';
+import * as yup from 'yup';
+import axios from 'axios'
+
 // images
 import noImage from '../../images/noImage.png';
 
+
+
+
 import './style.css';
 
-function Login() {
+const Login = () => {
+
+  const handleSubmit = values => console.log(values);
+
+  const validations =  yup.object().shape({
+    email: yup.string().email('O campo deve ser um Email').required('O campo é obrigatório'),
+    password: yup.string().required('O campo é obrigatório')
+    .matches(
+      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      "Teste de força 8 carecteres + 1 letra minuscula + 1 letra maiuscula + 1 NUMERO + 1 CARACTERE ESPERCIAL"
+    )
+  
+
+  })
+
   return (
     <div>
       <Header />
@@ -17,35 +38,39 @@ function Login() {
         <div className="imageContainer">
           <img src={noImage} alt="Zarpar" className="image" />
         </div>
-        <div className="formContainer">
-          <h1 className="formTitle">Login</h1>
-          <form className="form">
-            <input
-              type="text"
-              name="user"
-              className="formInput"
-              placeholder="CPF/CNPJ"
-            />
-            <br />
-            <input
-              type="password"
-              name="password"
-              className="formInput"
-              placeholder="Senha"
-            />
+        
+        <div className='formContainer'>
+        <Formik initialValues={{}} onSubmit={handleSubmit} validationSchema={validations}>
+          <Form className="Login">
+            <h1 className="formTitle">Login</h1>
+            <div className="Login-Group">
+              <Field name="email" className="Login-Field" placeholder="Email"/>
+              <div className="Login-Error-Container">
+              <ErrorMessage component="span" name="email" className="Login-error"/>
+              </div>
+            </div>
+            <div className="Login-Group">
+              <Field name="password" className="Login-Field" placeholder="Senha"/>
+              <div className="Login-Error-Container">
+              <ErrorMessage component="span" name="password" className="Login-error"/>
+              </div>
+              
+            </div>
             <div className="textLinkPasswordContainer">
               <Link to="/" className="textLinkPassword">
                 <p>Esqueceu a senha?</p>
               </Link>
             </div>
-            <div className="sendButtonContainer">
-              <input type="submit" value="Entrar" className="sendButton" />
-            </div>
-          </form>
-          <Link to="/Register" className="textLinkRegister">
-            <p>Ainda não tem um Cadastro? a gente já resolve isso</p>
-          </Link>
+            
+            <button className="Login-Btn" type="submit">Entrar</button>
+          </Form>
+        </Formik>
+        <Link to="/Register" className="textLinkRegister">
+          <p>Ainda não tem um Cadastro? a gente já resolve isso</p>
+        </Link>
+
         </div>
+
       </main>
       <div id="footer">
         <Footer />
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Switch, Router, Route } from 'react-router';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Register2 from './pages/Register2';
 
+import {history} from './history'
+import { BrowserRouter } from 'react-router-dom';
+
 function Routes() {
   return (
-    <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/Login" component={Login} />
-      <Route path="/Register" component={Register} />
-      <Route path="/EndRegister" component={Register2} />
+    <BrowserRouter history={history}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/Login" exact component={Login} />
+        <Route path="/Register" exact component={Register} />
+        <Route path="/EndRegister" exact component={Register2} />
+      </Switch>
     </BrowserRouter>
   );
 }
